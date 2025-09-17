cb5e84239aa0ac49ff65793b0251ce6c3c0b4bf1
ali <ali.hassan.salman.2000@gmail.com>
2025-09-17 16:46:14 +0300

    add show page

--- controllers/listings.js
+++ controllers/listings.js
@@ -3,8 +3,19 @@ const router = express.Router();
 
 const Listing = require('../models/listing');
 
-router.get('/', (req, res) => {
-    res.render('listings/index.ejs');
+router.get('/', async (req, res) => {
+  try {
+    const populatedListings = await Listing.find({}).populate('owner');
+    //console.log('Populated Listings:', populatedListings);
+    
+    res.render('listings/index.ejs', {
+        listings: populatedListings,
+    });
+  
+    } catch (error) {
+    console.log(error);
+    res.redirect('/');
+  }
 });
 
 router.get('/new', (req, res) => {
@@ -15,6 +26,21 @@ router.post('/', async (req, res) => {
   req.body.owner = req.session.user._id;
   await Listing.create(req.body);
   res.redirect('/listings');
+});
+
+router.get('/:listingId', async (req, res) => {
+  try {
+    const populatedListings = await Listing.findById(
+      req.params.listingId
+    ).populate('owner');
+
+    res.render('listings/show.ejs', {
+      listing: populatedListings,
+    });
+  } catch (error) {
+    console.log(error);
+    res.redirect('/');
+  }
 });
 
 
