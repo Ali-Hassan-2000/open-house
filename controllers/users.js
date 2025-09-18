45bc12c172d7d72aac98883327cecef0cbc15c6a
ali <ali.hassan.salman.2000@gmail.com>
2025-09-18 15:47:24 +0300

    add Profile

--- controllers/users.js
+++ controllers/users.js
@@ -5,12 +5,25 @@ const Listing = require('../models/listing.js');
 
 router.get('/profile', async (req, res) => {
   try {
-    console.log('user: ', req.session.user);
-    res.send('Profile page');
+    const myListings = await Listing.find({
+      owner: req.session.user._id,
+    }).populate('owner');
+
+    const myFavoriteListings = await Listing.find({
+      favoritedByUsers: req.session.user._id,
+    }).populate('owner');
+
+    res.render('users/show.ejs', {
+        myListings,
+        myFavoriteListings,
+    });
+
   } catch (error) {
     console.log(error);
+    res.redirect('/');
   }
 });
+
 
 
 
